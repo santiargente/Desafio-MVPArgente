20c176a6da65a6503808f11381d3562ea93a734e
Santiago Argente <santiagoargente@live.com.ar>
2022-11-07 21:38:45 -0300

    Se sube el desafío

--- MVPArgente/models.py
+++ MVPArgente/models.py
@@ -4,5 +4,6 @@ class Parientes(models.Model):
 
     nombre = models.CharField(max_length=40)
     parentezco = models.CharField(max_length=40)
+    fechanac = models.DateField()
     edad = models.IntegerField()
 
--- MVPArgente/views.py
+++ MVPArgente/views.py
@@ -4,8 +4,8 @@ from .models import Parientes
 
 def mostrar_parientes(request):
 
-    mama = Parientes(nombre= "Maria", parentezco= "Madre", edad = 45)
-    papa = Parientes(nombre= "Juan", parentezco= "Padre", edad= 50)
-    hermano = Parientes(nombre= "Carlos", parentezco= "Hermano", edad= 30)
+    mama = Parientes(nombre= "Maria", parentezco ="Mamá", fechanac= "1964-12-03", edad = 45)
+    papa = Parientes(nombre= "Juan", parentezco = "Papá", fechanac= "1972-20-09", edad= 50)
+    hermano = Parientes(nombre= "Carlos", parentezco = "Hermano", fechanac= "1992-05-08", edad= 30)
 
     return render(request, 'parientes.html', {'objetos': [mama, papa, hermano]})
